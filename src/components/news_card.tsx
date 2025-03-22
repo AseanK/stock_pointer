c3b57340b066ec66b9afc38b5f8081e3de44499b
Sean Kim <seank8809@gmail.com>
2025-03-22 23:48:59 +0900

    fetch data

--- src/components/news_card.tsx
+++ src/components/news_card.tsx
@@ -1,17 +1,49 @@
 'use client'
 
-import {db} from '@/utils/firebase.tsx' 
+import { db } from '@/utils/firebase'
 import React, { useEffect, useState } from 'react'
 import { collection, getDocs } from 'firebase/firestore'
 
-async function fetch_news() {
-	const querySnapshot = await getDocs(colletion(db, "news"));
-
-	const data = [];
-	querySnapshot.forEach((doc) => {
-		data.push(id: doc.id, ...doc.data());
-	})
-
-	return data;
+async function fetchNews() {
+	try {
+		const querySnapshot = await getDocs(collection(db, "news"));
+		const data = querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data()
+		}));
+		return data;
+	} catch (error) {
+		console.error("Error fetching news:", error);
+		return [];
+	}
 }
 
+const NewsList = () => {
+	const [news, setNews] = useState<any[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const loadNews = async () => {
+			const newsData = await fetchNews();
+			setNews(newsData);
+			setLoading(false);
+		};
+
+		loadNews();
+	}, []);
+
+	if (loading) return <p>Loading...</p>;
+
+	return (
+		<div>
+			<h1>News</h1>
+			<ul>
+				{news.map((item) => (
+					<li key={item.id}>{item.heading}</li> // Assuming `title` exists in Firestore
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default NewsList;
